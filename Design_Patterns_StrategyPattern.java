b4e59016ead3b39ffd864c4e455853b39da46c02
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-01-30 16:35:43 +0530

    Update Design_Patterns_StrategyPattern.java

--- Design_Patterns_StrategyPattern.java
+++ Design_Patterns_StrategyPattern.java
@@ -79,9 +79,8 @@ class paymentService {
         this.paymentStartegy.processPayment();
     }
   
- static void main(String args[]){
-   
-    
+public static void main(String args[]){
+ 
     paymentService first=new paymentService(new PaymentByUPI());
     
     paymentService second=new paymentService(new PaymentByCard());
